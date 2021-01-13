45883f68de0300af4bbc345de056980b568677c9
Jon Charter <jmcharter@gmail.com>
2021-01-13 15:03:10 +0000

    add default session method

--- models/wizard.py
+++ models/wizard.py
@@ -6,6 +6,13 @@ class Wizard(models.TransientModel):
     _name = 'openacademy.wizard'
     _description = "Wizard: Quick Registration of Attendees to Sessions"
 
-    sesssion_id = fields.Many2one('openacademy.session',
-        string="Session", required=True)
+    def _default_session(self):
+        return self.env['openacademy.session'].browse(self._context.get('active_id'))
+
+    session_id = fields.Many2one('openacademy.session',
+        string="Session", required=True, default=_default_session)
     attendee_ids = fields.Many2many('res.partner',string="Attendees")
+
+    def subscribe(self):
+        self.session_id.attendee_ids |= self.attendee_ids
+        return {}
